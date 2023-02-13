e37d033a4401735cb692786a41fe57792ab4ef1a
noreenh21 <hossain.n@northeastern.edu>
2023-02-13 15:12:16 -0500

    Circle color changes

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 
+// Change line color when button is clicked
 function buttonClicked(){
     let line = document.getElementById("line-element");
     line.setAttribute("style", "stroke:red;stroke-width:15");
+}
+
+// Get circle div
+let circle = document.getElementById("circle")
+
+// Set color when moused over
+function mouseOver(){
+    circle.setAttribute("fill" , "orange")
+}
+
+// Set color back when mouse out
+function mouseOut(){
+    circle.setAttribute("fill", "pink")
 }
